310a218e7dde744378dcd33dea817a445fa89dd3
Luka <luka.luka.luka93@gmail.com>
2025-06-10 15:50:46 -0700

    made it more desktop friendly

--- src/components/Services/services.jsx
+++ src/components/Services/services.jsx
@@ -12,49 +12,51 @@ export default function Services(){
 
     return (
         <>
-            <div className='service-wrapper'>
-                <h3>Services</h3>
-                <p>ICE AND ELECTRIC VEHICELS INCLUDING SERVICE RECORD DIRECTLY BY LAND ROVER AND JAGUAR DIAGNOSIS.
-                    <br/>
-                    -REPAIR AND COMPLEX REPAIRS INCLUDING COMPLEX ELECTRICAL FAULTS;
-                    <br/>
-                    -PREPERATION FOR MFK;
-                    <br/>
-                    -TUNING;
-                    <br/>
-                    -MODIFICATIONS
-                    <br/>
-                    -VEHICLE CHECK AND ASSISTING ADVICES IN BUYING LANDROVER OR JAGUAR VEHICLES.
-                </p>
-                <p id='free-coffee'>
-                    free coffee for customers
-                    <img src={coffeeGif} alt="coffee cup" className="coffee-gif" />  
-                </p>
-            </div>
-            <TiltCard
-                className="table first-table"
-                options={{
-                max: 20,
-                speed: 400,
-                glare: true,
-                "max-glare": 0.4,
-                }}
-                >
-                <div className='service2-rapper'>
-                    <div className='service-gif-name'>
-                        <img src={technician} alt='technician' />
-                        <p>Repairs</p>
-                    </div>
-                    <div className='service-gif-name'>
-                        <img src={carKey} alt='car key' />
-                        <p>4X4 Upgrades</p>
-                    </div>
-                    <div className='service-gif-name'>
-                        <img src={drill} alt='drill' />
-                        <p>Camping Gear</p>
-                    </div>
+            <div className='all-services-wrapper'>
+                <div className='service-wrapper'>
+                    <h3>Services</h3>
+                    <p>ICE AND ELECTRIC VEHICELS INCLUDING SERVICE RECORD DIRECTLY BY LAND ROVER AND JAGUAR DIAGNOSIS.
+                        <br/>
+                        -REPAIR AND COMPLEX REPAIRS INCLUDING COMPLEX ELECTRICAL FAULTS;
+                        <br/>
+                        -PREPERATION FOR MFK;
+                        <br/>
+                        -TUNING;
+                        <br/>
+                        -MODIFICATIONS
+                        <br/>
+                        -VEHICLE CHECK AND ASSISTING ADVICES IN BUYING LANDROVER OR JAGUAR VEHICLES.
+                    </p>
+                    <p id='free-coffee'>
+                        free coffee for customers
+                        <img src={coffeeGif} alt="coffee cup" className="coffee-gif" />  
+                    </p>
                 </div>
-            </TiltCard>
+                <TiltCard
+                    className="table first-table"
+                    options={{
+                    max: 20,
+                    speed: 400,
+                    glare: true,
+                    "max-glare": 0.4,
+                    }}
+                    >
+                    <div className='service2-rapper'>
+                        <div className='service-gif-name'>
+                            <img src={technician} alt='technician' />
+                            <p>Repairs</p>
+                        </div>
+                        <div className='service-gif-name'>
+                            <img src={carKey} alt='car key' />
+                            <p>4X4 Upgrades</p>
+                        </div>
+                        <div className='service-gif-name'>
+                            <img src={drill} alt='drill' />
+                            <p>Camping Gear</p>
+                        </div>
+                    </div>
+                </TiltCard>
+            </div>
         </>
     )
 }
