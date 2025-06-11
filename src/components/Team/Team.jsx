e52dea0655970c36ce6676176e3e7a0f09ff1c53
Luka <luka.luka.luka93@gmail.com>
2025-06-11 13:40:19 -0700

    added css

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -5,10 +5,10 @@ export default function Team(){
     return (
         <>
             <div className='team-wrapper'>
-                <h3>our<br/>TEAM</h3>
+                <h3>TEAM</h3>
                 <div className='our-team'>
                     <img src={rayan} alt='rajan ceo'/>
-                    <h4>Rayan Vujosevic - CEO</h4>
+                    <h4>Rayan Vujosevic - <span>CEO</span></h4>
                     <p>Originally from Serbia, Rayan is one of Europe's top auto mechanics, now based in Switzerland. With deep expertise in Jaguar and Land Rover vehicles, he specializes in advanced automotive electronics, diagnostics, and system repairs. Known for his precision, reliability, and passion for British engineering, Rayan brings over a decade of experience to every vehicle he touches — earning the trust of 4x4 drivers across Europe.</p>
                 </div>
             </div>
