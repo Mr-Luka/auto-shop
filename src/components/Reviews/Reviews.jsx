c1230f208052aa6ccbb025ca0e15b9eeba46d943
Luka <luka.luka.luka93@gmail.com>
2025-06-12 12:09:53 -0700

    added jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,12 +1,26 @@
 import './reviews.css';
+import {reviews} from './peopleReviews.js';
 
 export default function Reviews(){
 
     return (
-        <>
-            <div className='reviews-wrapper'>
-
+        <div className="reviews-wrapper">
+            {reviews.map((review, index) => (
+            <div key={index} className="review-card">
+                <div className="stars">
+                    {"★".repeat(Math.floor(review.rating))}
+                    {review.rating % 1 !== 0 && "☆"}
+                </div>
+                <p className="review-text">{review.text}</p>
+                <div className="reviewer">
+                    <img src={review.image} alt={review.name} />
+                    <div>
+                        <strong>{review.name}</strong>
+                        <p>{review.role}</p>
+                    </div>
+                </div>
             </div>
-        </>
+            ))}
+    </div>
     )
 }
