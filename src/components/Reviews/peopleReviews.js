ce64d327ec3642993229625fe2cfc455aa7098b1
Luka <luka.luka.luka93@gmail.com>
2025-06-12 12:37:44 -0700

    css reviews finished

--- src/components/Reviews/peopleReviews.js
+++ src/components/Reviews/peopleReviews.js
@@ -2,6 +2,8 @@ import face1 from '../../assets/testemonials/face1.jpg';
 import face2 from '../../assets/testemonials/face2.jpg';
 import face3 from '../../assets/testemonials/face3.jpg';
 import face4 from '../../assets/testemonials/face4.jpg';
+import face5 from '../../assets/testemonials/face5.jpg';
+import face6 from '../../assets/testemonials/face6.jpg';
 
 const reviews = [
     {
@@ -28,9 +30,23 @@ const reviews = [
     {
       name: "Mara",
       role: "Software Developer",
-      text: "Their crew was reliable and professional. They arrived on time each day and worked diligently to complete the project. They maintained a clean and organized worksite, and their communication was excellent. The quality of their workmanship is outstanding, and we are extremely satisfied with the results.",
+      text: "From the moment we contacted them, the process was smooth and professional. Their attention to detail and ability to explain each step of the repair was incredibly reassuring. I especially appreciated how respectful they were of my time and space.",
       rating: 5,
       image: face4
+    },
+    {
+      name: "Sean",
+      role: "Farmer",
+      text: "I’ve worked with many service providers over the years, and this team truly stands out. They handled my Land Rover with care and precision, and the results were outstanding. Reliable, honest, and skilled — I’d recommend them to anyone in need of top-tier auto service.",
+      rating: 5,
+      image: face5
+    },
+    {
+      name: "Kenny",
+      role: "Jaguar Specialist",
+      text: "It’s rare to find mechanics who truly understand Jaguars inside and out. These guys do. Their diagnostic skills and craftsmanship are top-notch, and the electrical work they did saved me thousands. Absolutely impressed with their expertise.",
+      rating: 5,
+      image: face6
     }
   ];
 
