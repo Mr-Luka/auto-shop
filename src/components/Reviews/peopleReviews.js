da31cbfa7b7d080b7689f55bb04c4438230c8ba5
Luka <luka.luka.luka93@gmail.com>
2025-06-12 12:07:12 -0700

    added data for reviews.js

--- src/components/Reviews/peopleReviews.js
+++ src/components/Reviews/peopleReviews.js
@@ -1,23 +1,37 @@
+import face1 from '../../assets/testemonials/face1.jpg';
+import face2 from '../../assets/testemonials/face2.jpg';
+import face3 from '../../assets/testemonials/face3.jpg';
+import face4 from '../../assets/testemonials/face4.jpg';
+
 const reviews = [
     {
-      name: "Mara",
+      name: "Artur",
       role: "Retired teacher",
       text: "We hired them for a full home renovation, and they exceeded our expectations in every way. The project was completed on time and within budget, with exceptional attention to detail. Their communication was excellent throughout, keeping us informed every step of the way. We highly recommend their services.",
       rating: 4.5,
-      image: mara
+      image: face1
     },
     {
-      name: "Sara",
+      name: "Max",
       role: "Accountant",
       text: "Their crew was punctual, courteous, and highly skilled. They maintained a clean and organized worksite, minimizing disruption to our daily life. The quality of their craftsmanship is evident in every aspect of the finished project. We are extremely satisfied with their work and would gladly hire them again.",
       rating: 5,
-      image: sara
+      image: face2
     },
     {
       name: "David",
       role: "Security",
       text: "Their crew was reliable and professional. They arrived on time each day and worked diligently to complete the project. They maintained a clean and organized worksite, and their communication was excellent. The quality of their workmanship is outstanding, and we are extremely satisfied with the results.",
       rating: 5,
-      image: david
+      image: face3
+    },
+    {
+      name: "Mara",
+      role: "Software Developer",
+      text: "Their crew was reliable and professional. They arrived on time each day and worked diligently to complete the project. They maintained a clean and organized worksite, and their communication was excellent. The quality of their workmanship is outstanding, and we are extremely satisfied with the results.",
+      rating: 5,
+      image: face4
     }
   ];
+
+export default reviews;
